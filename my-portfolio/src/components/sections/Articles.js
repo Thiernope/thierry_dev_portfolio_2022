a05d810dc6a79faec248602de757aa4c8fe4ad5d
Thiernope <ntirandth@gmail.com>
2022-10-24 11:21:52 +0200

    adding readings

--- my-portfolio/src/components/sections/Articles.js
+++ my-portfolio/src/components/sections/Articles.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom"
 import Typical from "react-typical"
 const Articles = () => {
   return (
@@ -12,30 +11,25 @@ const Articles = () => {
            wrapper = "p"
            /></h1>
     <div className="mt-10 md:mt-0 justify-items-center grid sm:grid-cols-2 gap-6 sm:gap-10 md:gap-16">
-   <Link to="" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CDN</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">CDN refers to Content Delivery Network or Content Distribution Network. It is a geographically distributed network of proxy servers and their data centers. It has the main goal of providing high avialablity and performane by distributin the services spatially relative to end users.<span className="text-green-500">read more</span></p>
-   </Link>
+   <a href="https://dev.to/thiernope/trigger-netlify-deploys-using-github-workflow-cicd-lcm" target="_blank" rel="noreferrer" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Trigger Netlify Deployments Using Github Workflow (CI/CD)</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">CDN refers to Content Delivery Network...<span className="text-green-500">read more</span></p>
+   </a>
 
-   <Link to="" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Next JS</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">Next.js is an open-source web development framework built on top of Node.js to enable React based web applications to have functionalities such as server-side rendering and generating static websites.<span className="text-green-500">read more</span></p>
-   </Link>
+   <a href="https://discovered-heat-57c.notion.site/Deploying-React-application-using-Node-js-and-nginx-on-an-Ubuntu-server-from-DigitalOcean-37e6404bd8ff400681dbe6768cd749f7" target="_blank" rel="noreferrer" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Deploying React application using Node.js and nginx on an Ubuntu server from DigitalOcean.</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">DititalOcean is one of the best cloude service...<span className="text-green-500">read more</span></p>
+   </a>
 
-   <Link to="" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CI/CD - Netlify</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">Have you wondered how you can deploy your react app on netlify as quickly as possible? Github Actions makes it easy and facilitates quick deployments using github workflow. In this post, I am going to show you how to trigger automatic deployments to netlify using github workflow.<span className="text-green-500">read more</span></p>
-   </Link>
+   <a href="https://discovered-heat-57c.notion.site/Understanding-universal-applications-c5c70d9dfa85474a98652c7470a39811" target="_blank" rel="noreferrer" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Understanding universal applications</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">A universal application is the application that run...<span className="text-green-500">read more</span></p>
+   </a>
 
-   <Link to="" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Hoisting in Javascript</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">JavaScript Hoisting refers to the process whereby declaration of functions, variables or classes are moved to the top of their scope, prior to execution of the code.<span className="text-green-500">read more</span></p>
-   </Link>
-
-   <Link to="" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PWA</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">A progressive web application, commonly known as a progressive web app, is a type of application software delivered through the web, built using common web technologies including HTML, CSS, JavaScript, and WebAssembly.<span className="text-green-500">read more</span></p>
-   </Link>
+   <a href="https://discovered-heat-57c.notion.site/Improving-the-Performance-of-Your-Applications-00c69483efdb4884b204c7cd983ea509" target="_blank" rel="noreferrer" class="block p-6 max-w-sm lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Improving the Performance of a React Applications</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">When it comes to performance optimization, react...<span className="text-green-500">read more</span></p>
+   </a>
     </div>
     </div>
   );
